faed031a931b45653a3ce82a32cc4e3b96151dea
mikeyb <pgp@mikes.email>
2025-05-19 06:05:43 -0700

    add fallback rpcs

--- app/viem-client.ts
+++ app/viem-client.ts
@@ -1,9 +1,14 @@
-import { createPublicClient, http } from 'viem';
+import { createPublicClient, fallback, http } from 'viem';
 import { base } from 'viem/chains';
 
 const client = createPublicClient({
     chain: base,
-    transport: http(),
+    transport: fallback([
+        http('wss://base-rpc.publicnode.com'),
+        http('https://base.llamarpc.com'),
+        http('https://base.drpc.org'),
+        http('https://base-pokt.nodies.app')
+    ]),
 });
 
 if (!client) {
